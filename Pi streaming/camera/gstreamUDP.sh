35c4e9237fb10fd91fb0dad58b17d003e5625dc3
Richard Ginus <richardginus@gmail.com>
2014-04-09 12:02:04 +0200

    adds status file creation in ram

--- Pi streaming/camera/gstreamUDP.sh
+++ Pi streaming/camera/gstreamUDP.sh
@@ -1,3 +1,6 @@
 raspivid -n -w 1280 -h 720 -b 2000000 -fps 24 -vf -hf -t 0 -o - | \
      gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! \
      udpsink host=192.168.1.6 port=9000
+status = "streaming"
+
+echo "status:$status" > /dev/shm/streamingstatus
