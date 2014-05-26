77f07681cd8d9bbf8bbdb2b8b7e5041deb322f43
remarknl <richardginus@gmail.com>
2014-05-26 18:04:20 +0200

    fix streaming script

--- Pi streaming/camera/gstreamUDP.sh
+++ Pi streaming/camera/gstreamUDP.sh
@@ -1,6 +1,6 @@
-raspivid -n -w 640 -h 48-0 -b 1000000 -fps 24 -vf -hf -t 0 -o - | \
+raspivid -n -w 640 -h 480 -b 1000000 -fps 24 -hf -t 0 -o - | \
      gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! \
-     udpsink host=videoserver.ulab.nl port=5001
+     udpsink host=videoserver.ulab.nl port=5004
 status = "streaming"
 
 echo "status:$status" > /dev/shm/streamingstatus
