778b11ee1b7f9a00e7e8ae46d943eca94c6aa7c6
Richard Ginus <richardginus@gmail.com>
2014-05-28 13:12:17 +0200

    fixed mirrored image on camera

--- Pi streaming/camera/gstreamUDP.sh
+++ Pi streaming/camera/gstreamUDP.sh
@@ -1,4 +1,4 @@
-raspivid -n -w 640 -h 480 -b 1000000 -fps 24 -hf -t 0 -o - | \
+raspivid -n -w 640 -h 480 -b 1000000 -fps 24 -t 0 -o - | \
      gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! \
      udpsink host=videoserver.ulab.nl port=5004
 status = "streaming"
